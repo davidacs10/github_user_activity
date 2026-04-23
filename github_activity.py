5a1add6e75eedbf83e31d8b096f028f73c2d6540
david <davidacs10.08@gmail.com>
2026-04-23 18:03:26 -0400

    Procesamiento y validacion de los datos.

--- github_activity.py
+++ github_activity.py
@@ -9,3 +9,11 @@ else:
 
 url = f"https://api.github.com/users/{user}/events"
 response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    print(len(data))
+elif response.status_code == 404:
+    print(f"El usuario: {user} no existe.")
+else:
+    print(f"Hubo un error {response.status_code}.")
