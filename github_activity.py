44a3b5c14c0c10d3fe4dc144eaf53a0ef9d88026
david <davidacs10.08@gmail.com>
2026-04-24 12:14:59 -0400

    Se agrego la estructura logica del programa

--- github_activity.py
+++ github_activity.py
@@ -12,7 +12,18 @@ response = requests.get(url)
 
 if response.status_code == 200:
     data = response.json()
-    print(len(data))
+    for event in data:
+        repo_name = event["repo"].get("name")
+        action = event["payload"].get("action", "unknown")
+        ref_type = event["payload"].get("ref_type", "unknown")
+        if event["type"] == "PushEvent":
+            print(f"Pushed commits to {repo_name}")
+        elif event["type"] == "CreateEvent":
+            print(f"Create a new {ref_type} in {repo_name}")
+        elif event["type"] == "WatchEvent":
+            print(f"Starred {repo_name}")
+        elif event["type"] == "IssuesEvent":
+            print(f"Issue {action} in {repo_name}")
 elif response.status_code == 404:
     print(f"El usuario: {user} no existe.")
 else:
